e6864ec6b5f2d8bb345ca3df07be4ce00cbbf62e
Anna Lin <annalin1510@outlook.com>
2026-01-21 17:04:21 +0000

    3d flip interaction for front card and back card

---
+++ script.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll(".card");
+
+function flipCard(e) {
+  console.log(e.target);
+}
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
