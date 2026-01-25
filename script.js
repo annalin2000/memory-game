dd1dc7a951f704af549a9f51092a804f61931143
Anna Lin <annalin1510@outlook.com>
2026-01-25 22:50:06 +0000

    adjusted settime out

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ function checkMatch(img1, img2) {
     cardOne.classList.remove("shake", "flip");
     cardTwo.classList.remove("shake", "flip");
     resetTurn();
-  }, 380);
+  }, 600);
 }
 
 cards.forEach((card) => {
